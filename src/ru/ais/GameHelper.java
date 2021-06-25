f3218e797db68e3e4e0c8d86999c87b58117cfa4
Vadim <gimee@mail.ru>
2021-06-25 21:21:19 +0300

    New

--- src/ru/ais/GameHelper.java
+++ src/ru/ais/GameHelper.java
@@ -7,17 +7,27 @@ import java.util.Comparator;
 
 public class GameHelper {
     public List<Integer> moveAndMergeEqual(List<Integer> list) {
-        Collections.sort(list, Comparator.nullsLast(Comparator.naturalOrder()));
-        List<Integer> list1 = new ArrayList<>(list);
+        //Collections.sort(list, Comparator.nullsLast(Comparator.naturalOrder()));
+        List<Integer> list1 = new ArrayList<>();
+        int k = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == null){
+                list1.add(null);
+            } else {
+                list1.add(k, list.get(i));
+                k++;
+            }
+        }
         for (int i = 0; i < list1.size()-1; i++) {
             if (list1.get(i) != null) {
                 if (list1.get(i) == list1.get(i + 1)) {
-                    list1.set(i, list1.get(i)*2);
-                    list1.remove(i+1);
+                    list1.set(i, list1.get(i) * 2);
+                    list1.remove(i + 1);
                     list1.add(null);
                 }
             }
         }
+
         list = list1;
         return list;
         }
